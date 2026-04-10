37097e40c3a0c534facea5c4a9dc739ac3c2fb7b
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-11 01:01:41 +0200

    create Connection of DataBase in AwladAli_Data -> clsDataAccessSettings and add clsUserData in DataLayer and add clsUser in BuisnessLayer

---
+++ Code/AwladAli_Buisness/clsUser.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data;
+using AwladAli_Data;
+
+namespace AwladAli_Buisness
+{
+    public class clsUser
+    {
+        // Enums to define the mode of the object
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        // User Properties
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+
+        // Default constructor for AddNew mode
+        public clsUser()
+        {
+            this.UserID = -1;
+            this.UserName = "";
+            this.Password = "";
+            this.Role = -1;
+
+            Mode = enMode.AddNew;
+        }
+
+        // Private constructor for Update mode (used internally by Find method)
+        private clsUser(int UserID, string UserName, string Password, int Role)
+        {
+            this.UserID = UserID;
+            this.UserName = UserName;
+            this.Password = Password;
+            this.Role = Role;
+
+            Mode = enMode.Update;
+        }
+
+        // 1. Find User by ID
+        public static clsUser Find(int UserID)
+        {
+            string UserName = "";
+            string Password = "";
+            int Role = -1;
+
+            if (clsUserData.GetUserByID(UserID, ref UserName, ref Password, ref Role))
+            {
+                return new clsUser(UserID, UserName, Password, Role);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // 2. Add New User (Internal helper)
+        private bool _AddNewUser()
+        {
+            this.UserID = clsUserData.AddNewUser(this.UserName, this.Password, this.Role);
+            return (this.UserID != -1);
+        }
+
+        // 3. Update User (Internal helper)
+        private bool _UpdateUser()
+        {
+            return clsUserData.UpdateUser(this.UserID, this.UserName, this.Password, this.Role);
+        }
+
+        // 4. Save Method: Routes to Add or Update based on Mode
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewUser())
+                    {
+                        Mode = enMode.Update; // Change mode after successful save
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateUser();
+            }
+
+            return false;
+        }
+
+        // 5. Delete User (Static)
+        public static bool DeleteUser(int UserID)
+        {
+            return clsUserData.DeleteUser(UserID);
+        }
+
+        // 6. Get All Users (Static)
+        public static DataTable GetAllUsers()
+        {
+            return clsUserData.GetAllUsers();
+        }
+    }
+}
---
+++ Code/AwladAli_Data/clsUserData.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
+namespace AwladAli_Data
+{
+    public class clsUserData
+    {
+        // 1. Find User by ID
+        public static bool GetUserByID(int UserID, ref string UserName, ref string Password, ref int Role)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM Users WHERE UserID = @UserID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserID", UserID);
+                        connection.Open();
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                UserName = (string)reader["UserName"];
+                                Password = (string)reader["Password"];
+                                Role = Convert.ToInt32(reader["Role"]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+                // Log exception here if needed
+            }
+
+            return isFound;
+        }
+
+        // 2. Add New User
+        public static int AddNewUser(string UserName, string Password, int Role)
+        {
+            int UserID = -1;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"INSERT INTO Users (UserName, Password, Role) 
+                                     VALUES (@UserName, @Password, @Role);
+                                     SELECT last_insert_rowid();";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserName", UserName);
+                        command.Parameters.AddWithValue("@Password", Password);
+                        command.Parameters.AddWithValue("@Role", Role);
+
+                        connection.Open();
+
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            UserID = insertedID;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return UserID;
+        }
+
+        // 3. Update User
+        public static bool UpdateUser(int UserID, string UserName, string Password, int Role)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"UPDATE Users 
+                                     SET UserName = @UserName, 
+                                         Password = @Password, 
+                                         Role = @Role 
+                                     WHERE UserID = @UserID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserID", UserID);
+                        command.Parameters.AddWithValue("@UserName", UserName);
+                        command.Parameters.AddWithValue("@Password", Password);
+                        command.Parameters.AddWithValue("@Role", Role);
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        // 4. Delete User
+        public static bool DeleteUser(int UserID)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "DELETE FROM Users WHERE UserID = @UserID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserID", UserID);
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        // 5. Get All Users (Returns DataTable for UI Grids)
+        public static DataTable GetAllUsers()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM Users";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                dt.Load(reader);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return dt;
+        }
+    }
+}
