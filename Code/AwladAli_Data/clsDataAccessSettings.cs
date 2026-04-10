f772ed0e10108fd563f54495551b6f3f1fbb0d93
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-11 00:50:49 +0200

    Cleanup: Ignore build folders and vs settings

---
+++ Code/AwladAli_Data/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwladAli_Data
+{
+    public class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Data Source=AwladAli.db;Version=3;";
+    }
+}
