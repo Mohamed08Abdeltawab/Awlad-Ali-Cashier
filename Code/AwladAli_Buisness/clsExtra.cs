f3dc9c466a19d4064bf5df05dccfe5d0966524cf
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-11 11:00:24 +0200

    add clsEtra in Buisness Layer

---
+++ Code/AwladAli_Buisness/clsExtra.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using AwladAli_Data;
+
+namespace AwladAli_Buisness
+{
+    public class clsExtra
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int ExtraID { get; set; }
+        public string ExtraName { get; set; }
+        public double Price { get; set; }
+
+        public clsExtra()
+        {
+            this.ExtraID = -1;
+            this.ExtraName = "";
+            this.Price = 0.0;
+            Mode = enMode.AddNew;
+        }
+
+        private clsExtra(int ExtraID, string ExtraName, double Price)
+        {
+            this.ExtraID = ExtraID;
+            this.ExtraName = ExtraName;
+            this.Price = Price;
+            Mode = enMode.Update;
+        }
+
+        public static clsExtra Find(int ExtraID)
+        {
+            string Name = ""; double Price = 0.0;
+            if (clsExtraData.GetExtraByID(ExtraID, ref Name, ref Price))
+                return new clsExtra(ExtraID, Name, Price);
+            return null;
+        }
+
+        private bool _AddNew() => (this.ExtraID = clsExtraData.AddNewExtra(this.ExtraName, this.Price)) != -1;
+        private bool _Update() => clsExtraData.UpdateExtra(this.ExtraID, this.ExtraName, this.Price);
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew()) { Mode = enMode.Update; return true; }
+                    return false;
+                case enMode.Update:
+                    return _Update();
+            }
+            return false;
+        }
+
+        public static bool Delete(int ExtraID) => clsExtraData.DeleteExtra(ExtraID);
+        public static DataTable GetAllExtras() => clsExtraData.GetAllExtras();
+    }
+}
