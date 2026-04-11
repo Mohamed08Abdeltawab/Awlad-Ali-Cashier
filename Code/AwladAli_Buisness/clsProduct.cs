ff9ebc412529018f4bed3a3f47dbc979a7c5ac38
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-11 10:52:40 +0200

    add Product Classes in Data Layer and Business layer

---
+++ Code/AwladAli_Buisness/clsProduct.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using AwladAli_Data;
+
+namespace AwladAli_Buisness
+{
+    public class clsProduct
+    {
+        // Enums to define the mode of the object
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        // Product Properties
+        public int ProductID { get; set; }
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+
+        // Navigation Property: To get Category details easily
+        public clsCategory CategoryInfo;
+
+        // Default constructor for AddNew mode
+        public clsProduct()
+        {
+            this.ProductID = -1;
+            this.CategoryID = -1;
+            this.ProductName = "";
+
+            Mode = enMode.AddNew;
+        }
+
+        // Private constructor for Update mode
+        private clsProduct(int ProductID, int CategoryID, string ProductName)
+        {
+            this.ProductID = ProductID;
+            this.CategoryID = CategoryID;
+            this.ProductName = ProductName;
+
+            // Load Category Information
+            this.CategoryInfo = clsCategory.Find(CategoryID);
+
+            Mode = enMode.Update;
+        }
+
+        // 1. Find Product by ID
+        public static clsProduct Find(int ProductID)
+        {
+            int CategoryID = -1;
+            string ProductName = "";
+
+            if (clsProductData.GetProductByID(ProductID, ref CategoryID, ref ProductName))
+            {
+                return new clsProduct(ProductID, CategoryID, ProductName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // 2. Add New Product (Internal helper)
+        private bool _AddNewProduct()
+        {
+            this.ProductID = clsProductData.AddNewProduct(this.CategoryID, this.ProductName);
+            return (this.ProductID != -1);
+        }
+
+        // 3. Update Product (Internal helper)
+        private bool _UpdateProduct()
+        {
+            return clsProductData.UpdateProduct(this.ProductID, this.CategoryID, this.ProductName);
+        }
+
+        // 4. Save Method: Routes to Add or Update
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewProduct())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateProduct();
+            }
+
+            return false;
+        }
+
+        // 5. Delete Product (Static)
+        public static bool DeleteProduct(int ProductID)
+        {
+            return clsProductData.DeleteProduct(ProductID);
+        }
+
+        // 6. Get All Products (Static)
+        public static DataTable GetAllProducts()
+        {
+            return clsProductData.GetAllProducts();
+        }
+
+        // 7. Check if Category has any Products (Helper for Category Delete logic)
+        public static bool DoesCategoryHaveProducts(int CategoryID)
+        {
+            return clsCategoryData.IsCategoryLinkedToProducts(CategoryID);
+        }
+    }
+}
---
+++ Code/AwladAli_Data/clsProductData.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
+namespace AwladAli_Data
+{
+    public class clsProductData
+    {
+        // 1. Find Product by ID
+        public static bool GetProductByID(int ProductID, ref int CategoryID, ref string ProductName)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM Products WHERE ProductID = @ProductID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductID", ProductID);
+                        connection.Open();
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                CategoryID = Convert.ToInt32(reader["CategoryID"]);
+                                ProductName = (string)reader["ProductName"];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception) { isFound = false; }
+
+            return isFound;
+        }
+
+        // 2. Add New Product
+        public static int AddNewProduct(int CategoryID, string ProductName)
+        {
+            int ProductID = -1;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"INSERT INTO Products (CategoryID, ProductName) 
+                                     VALUES (@CategoryID, @ProductName);
+                                     SELECT last_insert_rowid();";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        command.Parameters.AddWithValue("@ProductName", ProductName);
+
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            ProductID = insertedID;
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return ProductID;
+        }
+
+        // 3. Update Product
+        public static bool UpdateProduct(int ProductID, int CategoryID, string ProductName)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"UPDATE Products 
+                                     SET CategoryID = @CategoryID, 
+                                         ProductName = @ProductName 
+                                     WHERE ProductID = @ProductID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductID", ProductID);
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        command.Parameters.AddWithValue("@ProductName", ProductName);
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception) { return false; }
+
+            return (rowsAffected > 0);
+        }
+
+        // 4. Delete Product
+        public static bool DeleteProduct(int ProductID)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductID", ProductID);
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return (rowsAffected > 0);
+        }
+
+        // 5. Get All Products (Joining with Category Name for better UI display)
+        public static DataTable GetAllProducts()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    // We join with Categories to show the Name instead of just ID in the Grid
+                    string query = @"SELECT Products.ProductID, Products.ProductName, Categories.CategoryName 
+                                     FROM Products 
+                                     INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows) dt.Load(reader);
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return dt;
+        }
+    }
+}
