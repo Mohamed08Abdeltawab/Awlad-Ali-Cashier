cb66e54a8a0a01456bcc9796fd28afb52f5b9d02
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-11 10:45:05 +0200

    create Category Classes in Data Layer and Buisness Layer

---
+++ Code/AwladAli_Buisness/clsCategory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using AwladAli_Data;
+
+namespace AwladAli_Buisness
+{
+    public class clsCategory
+    {
+        // Enums to define the mode of the object
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        // Category Properties
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        // Default constructor for AddNew mode
+        public clsCategory()
+        {
+            this.CategoryID = -1;
+            this.CategoryName = "";
+
+            Mode = enMode.AddNew;
+        }
+
+        // Private constructor for Update mode
+        private clsCategory(int CategoryID, string CategoryName)
+        {
+            this.CategoryID = CategoryID;
+            this.CategoryName = CategoryName;
+
+            Mode = enMode.Update;
+        }
+
+        // 1. Find Category by ID
+        public static clsCategory Find(int CategoryID)
+        {
+            string CategoryName = "";
+
+            if (clsCategoryData.GetCategoryByID(CategoryID, ref CategoryName))
+            {
+                return new clsCategory(CategoryID, CategoryName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // 2. Add New Category (Internal helper)
+        private bool _AddNewCategory()
+        {
+            this.CategoryID = clsCategoryData.AddNewCategory(this.CategoryName);
+            return (this.CategoryID != -1);
+        }
+
+        // 3. Update Category (Internal helper)
+        private bool _UpdateCategory()
+        {
+            return clsCategoryData.UpdateCategory(this.CategoryID, this.CategoryName);
+        }
+
+        // 4. Save Method: Managed logic for Add/Update
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCategory())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateCategory();
+            }
+
+            return false;
+        }
+
+        // 5. Delete Category (Static)
+        public static bool DeleteCategory(int CategoryID)
+        {
+            // Logic: Refuse deletion if there are products under this category
+            if (clsCategoryData.IsCategoryLinkedToProducts(CategoryID))
+            {
+                // You can also throw a custom exception here to be caught in UI
+                return false;
+            }
+
+            return clsCategoryData.DeleteCategory(CategoryID);
+        }
+
+        // 6. Get All Categories (Static)
+        public static DataTable GetAllCategories()
+        {
+            return clsCategoryData.GetAllCategories();
+        }
+    }
+}
---
+++ Code/AwladAli_Data/clsCategoryData.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
+namespace AwladAli_Data
+{
+    public class clsCategoryData
+    {
+        // 1. Find Category by ID
+        public static bool GetCategoryByID(int CategoryID, ref string CategoryName)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT CategoryName FROM Categories WHERE CategoryID = @CategoryID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        connection.Open();
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                CategoryName = (string)reader["CategoryName"];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+                // Log exception if needed
+            }
+
+            return isFound;
+        }
+
+        // 2. Add New Category
+        public static int AddNewCategory(string CategoryName)
+        {
+            int CategoryID = -1;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"INSERT INTO Categories (CategoryName) 
+                                     VALUES (@CategoryName);
+                                     SELECT last_insert_rowid();";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryName", CategoryName);
+
+                        connection.Open();
+
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            CategoryID = insertedID;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return CategoryID;
+        }
+
+        // 3. Update Category
+        public static bool UpdateCategory(int CategoryID, string CategoryName)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"UPDATE Categories 
+                                     SET CategoryName = @CategoryName 
+                                     WHERE CategoryID = @CategoryID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        command.Parameters.AddWithValue("@CategoryName", CategoryName);
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        // 4. Delete Category
+        public static bool DeleteCategory(int CategoryID)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "DELETE FROM Categories WHERE CategoryID = @CategoryID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        // 5. Get All Categories
+        public static DataTable GetAllCategories()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM Categories";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                dt.Load(reader);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+            }
+
+            return dt;
+        }
+
+
+        // Check if Category is linked to any products before deletion
+        public static bool IsCategoryLinkedToProducts(int CategoryID)
+        {
+            bool isLinked = false;
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    // We just need to know if at least one product exists
+                    string query = "SELECT 1 FROM Products WHERE CategoryID = @CategoryID LIMIT 1";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CategoryID", CategoryID);
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        isLinked = (result != null);
+                    }
+                }
+            }
+            catch (Exception) { isLinked = true; } // Safety first: if error, assume linked to prevent accidental delete
+            return isLinked;
+        }
+    }
+}
